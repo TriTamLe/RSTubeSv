18e7713396303756d4b83dbae418b69375971702
TriTamLe <tritam.le54@gmail.com>
2023-02-16 17:04:14 +0700

    push env

--- src/config/database/index.db.js
+++ src/config/database/index.db.js
@@ -4,10 +4,7 @@ require('dotenv').config();
 async function connect() {
   try {
     mongoose.set('strictQuery', true);
-    await mongoose.connect(
-      `mongodb://${process.env.DB_HOST}/${process.env.DB_COLLECTION}`,
-      {},
-    );
+    await mongoose.connect(process.env.DB_URL, {});
     console.log('Connected to database');
   } catch (error) {
     console.log('Connecting failed!', error);
