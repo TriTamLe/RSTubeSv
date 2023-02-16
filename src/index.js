fbf25eb2c17e88602271f1c3a81b25e76b8aa881
TriTamLe <tritam.le54@gmail.com>
2023-02-16 17:34:36 +0700

    remove port

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 require('dotenv').config();
 const app = express();
-const PORT = process.env.HOST_PORT || 3001;
+const PORT = process.env.HOST_PORT;
 const route = require('./routes/index.route');
 const bodyParser = require('body-parser');
 const cors = require('cors');
